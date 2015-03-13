60d3d5b869871d2e8f545bdcd1681f1172399ee8
cskilbeck <github@charlie.skilbeck.com>
2015-03-13 20:06:47 +0000

    no message

---
+++ Who/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using System.Web.Mvc;
+using Who.Models;
+using Microsoft.VisualBasic.FileIO;
+
+namespace Who.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            AccountSet a = new AccountSet();
+            try
+            {
+                using (TextFieldParser parser = new TextFieldParser("D:\\temp.csv"))
+                {
+                    parser.TextFieldType = FieldType.Delimited;
+                    parser.SetDelimiters(",");
+                    while (!parser.EndOfData)
+                    {
+                        string[] values = parser.ReadFields();
+                        for (int i = values.Length; i < 4; ++i)
+                        {
+                            values[i] = "";
+                        }
+                        a.Accounts.Add(new AccountSet.Account
+                        {
+                            Name = values[0],
+                            DAM = values[1],
+                            AM = values[2],
+                            RM = values[3]
+                        });
+                    }
+                } 
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                a.Error = "Data file not found";
+            }
+            catch (System.IO.IOException)
+            {
+                a.Error = "IO Error loading data file";
+            }
+            return View(a);
+        }
+    }
+}
---
+++ Who/Models/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Who.Models
+{
+    public class AccountSet
+    {
+        public class Account : IComparable
+        {
+            public string Name { get; set; }
+            public string DAM { get; set; }
+            public string AM { get; set; }
+            public string RM { get; set; }
+
+            public int CompareTo(object obj)
+            {
+                return Name.CompareTo(((Account)obj).Name);
+            }
+        }
+
+        public string Error { get; set; }
+        public SortedSet<Account> Accounts = new SortedSet<Account>();
+    }
+}
